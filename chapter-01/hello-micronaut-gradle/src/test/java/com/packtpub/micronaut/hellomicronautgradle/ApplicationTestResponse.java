7b2f2b68c9ac75f03496bcd1d496b530bab3c619
Zack Dawood <zackria@gmail.com>
2020-08-23 17:55:51 -0400

    Modifying test cases

---
+++ chapter-01/hello-micronaut-gradle/src/test/java/com/packtpub/micronaut/hellomicronautgradle/ApplicationTestResponse.java
@@ -0,0 +1,30 @@
+package com.packtpub.micronaut.hellomicronautgradle;
+
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.client.RxHttpClient;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.test.annotation.MicronautTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+
+@MicronautTest
+class ApplicationTestResponse {
+
+    @Inject
+    @Client("/")
+
+    RxHttpClient client;
+
+    @Test
+    void testResponseMessage() {
+
+        HttpRequest<String> request = HttpRequest.GET("/");
+        String body = client.toBlocking().retrieve(request);
+
+        Assertions.assertNotNull(body);
+        Assertions.assertEquals("Hello, PackT Micronaut!", body);
+
+    }
+}
