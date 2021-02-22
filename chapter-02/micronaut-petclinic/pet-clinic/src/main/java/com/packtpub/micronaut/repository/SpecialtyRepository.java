0ca6704a3855206a166eea7900aea2090eca4ed4
Nirmal <nirm.singh@cgi.com>
2021-02-22 14:57:21 -0500

    chapter-02

--- chapter-02/micronaut-petclinic/pet-clinic/src/main/java/com/packtpub/micronaut/repository/SpecialtyRepository.java
+++ chapter-02/micronaut-petclinic/pet-clinic/src/main/java/com/packtpub/micronaut/repository/SpecialtyRepository.java
@@ -1,7 +1,10 @@
 package com.packtpub.micronaut.repository;
 
 import com.packtpub.micronaut.domain.Specialty;
-import org.apache.ibatis.annotations.*;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.Collection;
 import java.util.Set;
@@ -20,10 +23,16 @@ public interface SpecialtyRepository {
     @Select("SELECT * FROM petclinic.specialties WHERE UPPER(name) = #{name}")
     Specialty findByName(@Param("name") String name) throws Exception;
 
-    @Select({
-            "INSERT INTO petclinic.specialties(name) values(#{name})",
-            "RETURNING id"
-    })
+    @Select(
+        {
+            "INSERT INTO petclinic.specialties(id, name)\n" +
+                "VALUES (COALESCE(#{id}, (select nextval('petclinic.specialties_id_seq'))), #{name})\n" +
+                "ON CONFLICT (id)\n" +
+                "DO UPDATE SET name = #{name}  \n" +
+                "WHERE petclinic.specialties.id = #{id}\n" +
+                "RETURNING id"
+        }
+    )
     @Options(flushCache = Options.FlushCachePolicy.TRUE)
     Long save(Specialty specialty) throws Exception;
 
@@ -31,9 +40,9 @@ public interface SpecialtyRepository {
     void deleteById(@Param("id") Long id) throws Exception;
 
     @Select({
-            "SELECT DISTINCT id, name FROM petclinic.specialties WHERE id IN(",
-            "SELECT specialty_id FROM petclinic.vet_specialties WHERE vet_id = #{vetId}",
-            ")"
+        "SELECT DISTINCT id, name FROM petclinic.specialties WHERE id IN(",
+        "SELECT specialty_id FROM petclinic.vet_specialties WHERE vet_id = #{vetId}",
+        ")"
     })
     Set<Specialty> findByVetId(@Param("vetId") Long vetId) throws Exception;
 }
