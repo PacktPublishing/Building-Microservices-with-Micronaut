bdeb3ddac6340aeaf5af02f92de55e499945f071
Zack Dawood <zackria@gmail.com>
2021-05-02 18:44:24 -0400

    Chapter 10 Updated
    Chapter 10 Updated

--- chapter-10/micronaut-petclinic/petclinic-alexa-maven/src/main/java/com/packtpub/alexa/PetClinicWelcomeIntentHandler.java
+++ chapter-10/micronaut-petclinic/petclinic-alexa-maven/src/main/java/com/packtpub/alexa/PetClinicWelcomeIntentHandler.java
@@ -1,11 +1,9 @@
 package com.packtpub.alexa;
 
-
 import com.amazon.ask.dispatcher.request.handler.HandlerInput;
 import com.amazon.ask.dispatcher.request.handler.RequestHandler;
 import com.amazon.ask.model.Response;
 import java.util.Optional;
-
 import static com.amazon.ask.request.Predicates.intentName;
 
 public class PetClinicWelcomeIntentHandler implements RequestHandler {
